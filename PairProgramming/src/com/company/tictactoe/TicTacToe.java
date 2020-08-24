d0216c12532cbec600276e844bdaedc9637f09df
chundh <dhchun0716@gmail.com>
2020-08-24 17:53:32 +0900

    Update TicTacToe

--- PairProgramming/src/com/company/tictactoe/TicTacToe.java
+++ PairProgramming/src/com/company/tictactoe/TicTacToe.java
@@ -6,6 +6,7 @@ public class TicTacToe {
     static int[][] arr = new int[3][3];
     Player p1;
     Player p2;
+    int cnt=0;
     int n;
     Scanner sc = new Scanner(System.in);
     public void start(){
@@ -20,8 +21,8 @@ public class TicTacToe {
                 int y = 0;
                 if(flag==1) {
                     if (p1.getInfo().equals("AI")) {
-                        x = (int) (Math.random() * 2);
-                        y = (int) (Math.random() * 2);
+                        x = (int) (Math.random() * 3);
+                        y = (int) (Math.random() * 3);
                     } else {
                         System.out.println("좌표를 입력해주세요");
                         x = sc.nextInt();
@@ -30,19 +31,21 @@ public class TicTacToe {
                 }
                 else{
                     if (p2.getInfo().equals("AI")) {
-                        x = (int) (Math.random() * 2);
-                        y = (int) (Math.random() * 2);
+                        x = (int) (Math.random() * 3);
+                        y = (int) (Math.random() * 3);
                     } else {
                         System.out.println("좌표를 입력해주세요");
                         x = sc.nextInt();
                         y = sc.nextInt();
                     }
                 }
+                System.out.println(x + " , " + y);
                 if (flag == 1) {
                     if (arr[x][y] != 0) {
                         System.out.println("해당위치에 돌이 존재합니다.");
                     } else {
                         arr[x][y] = 1;
+                        this.cnt++;
                         if(this.getResult(p1, 1)){
                             System.out.println("Player1이 승리했습니다.");
                             p1.setWinNum(p1.getWinNum()+1);
@@ -56,6 +59,7 @@ public class TicTacToe {
                         System.out.println("해당위치에 돌이 존재합니다.");
                     } else {
                         arr[x][y] = -1;
+                        this.cnt++;
                         if(this.getResult(p2, -1)){
                             System.out.println("Player2가 승리했습니다.");
                             p2.setWinNum(p2.getWinNum()+1);
@@ -69,6 +73,12 @@ public class TicTacToe {
         }
     }
     public boolean getResult(Player p, int flag){
+        if(this.cnt==(arr.length*arr.length)) {
+            System.out.println("결과가 나오지 않으므로 게임을 처음부터 시작합니다.");
+            arr = new int[3][3];
+            cnt=0;
+            return false;
+        }
         int cnt=0;
         int cnt1=0;
         for(int i=0; i<arr.length; i++){ // 가로, 세로로 같은지 검사
