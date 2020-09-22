a462b25da5510609d95a2e650e5b144e59599bd0
chundh <dhchun0716@gmail.com>
2020-09-22 22:47:06 +0900

    Lambda_Homework Update

--- 5_JavaAdvanced/src/com/company/day3/Homework/LambdaExpression.java
+++ 5_JavaAdvanced/src/com/company/day3/Homework/LambdaExpression.java
@@ -1,6 +1,7 @@
 package com.company.day3.Homework;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * 람다식을 이용하여 알고리즘 정답을 테스트하는 프로그램을 작성하시오.
@@ -17,11 +18,7 @@ interface Solution<T, R> {
 class Algorithm<T, R> {
     boolean test(T input, R groundtruth, Solution<? super T, ? super R> solution) {
         // TODO: solution을 실행하고, 이것이 정답(groundtruth)와 일치하는지 테스트하여 일치 여부를 출력.
-        if(solution.solve(input) == groundtruth) {
-            return true;
-        }else {
-            return false;
-        }
+        return solution.solve(input) == groundtruth;
     }
 }
 
@@ -29,17 +26,26 @@ public class LambdaExpression {
     public static void main(String[] args) {
         // TODO: test를 이용하여 알고리즘 문제를 하나 이상 풀이하고 테스트 결과를 출력하시오.
         Algorithm<Integer, Integer> algorithm = new Algorithm<>();
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(algorithm.test(a, b, s -> solution(a,b)));
+        int n = 6;
+        int ans = 5;
+        System.out.println(algorithm.test(n, ans, s -> solution(n,ans)));
     }
 
     public static int solution(int a, int b){
-        if(a==b){
-            return b;
+        int n = a;
+        Stack<Integer> arr = new Stack<>();
+        int[] dp = {10, 3, 7, 4, 12, 2};
+        long answer = 0;
+        for(int i=0; i<n; i++){
+            int data = dp[i];
+            while(arr.size()!=0 && arr.peek()<=data) arr.pop();
+            answer+=arr.size();
+            arr.add(data);
+        }
+        if(b == answer){
+            return (int) answer;
         }else{
-            return a;
+            return -1;
         }
     }
 }
